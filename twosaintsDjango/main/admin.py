b3970853305070ce25ff0b9558de83770afeb320
Choi <bchoi5@esm246.esm.rochester.edu>
2018-01-04 09:37:52 -0500

    css and html changes

--- twosaintsDjango/main/admin.py
+++ twosaintsDjango/main/admin.py
@@ -4,11 +4,15 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Event
 from .models import Announcement
+from .models import Blog
 
 class AnnouncementAdmin(admin.ModelAdmin):
 	list_display = ['date', 'title', 'body', 'upload']
 class EventAdmin(admin.ModelAdmin):
 	list_display = ['date', 'time', 'title', 'description']
+class BlogAdmin(admin.ModelAdmin):
+	list_display = ['date', 'title', 'body', 'upload']
 
 admin.site.register(Announcement, AnnouncementAdmin)
 admin.site.register(Event, EventAdmin)
+admin.site.register(Blog, BlogAdmin)
