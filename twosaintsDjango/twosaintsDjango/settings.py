eba800aa6620a3e615a2b148a33f5f629cf96f68
chuisu <choi.brandon.h@gmail.com>
2018-01-06 13:01:51 -0500

    Fixing static settings.py

--- twosaintsDjango/twosaintsDjango/settings.py
+++ twosaintsDjango/twosaintsDjango/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-# STATIC_URL = os.path.join(BASE_DIR, 'twosaintsDjango', '/static/')
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'twosaintsDjango', '/static/')
+# STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, 'twosaintsDjango', 'static'),
